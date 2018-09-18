e55fcd272abaa949c1d585899b0c52379afb54f7
Natalia Khrustaleva <hrustalyova@mail.ru>
2018-09-18 21:28:41 +0300

    Create 'Add post' window

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,6 +7,7 @@ import {LoginComponent} from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { ShowPostComponent } from './show-post/show-post.component';
+import { AddPostComponent } from './add-post/add-post.component';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { ShowPostComponent } from './show-post/show-post.component';
     RootComponent,
     LoginComponent,
     HomeComponent,
-    ShowPostComponent
+    ShowPostComponent,
+    AddPostComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/models/post.model.ts
@@ -0,0 +1,14 @@
+export class Post {
+	constructor(){
+		this.id = '';
+        this.title = '';//название статьи
+        this.pic = '';//картинка
+        this.description = '';//текст статьи
+        this.category='';//категория
+	}
+	public id;
+    public title;
+    public pic;
+    public description;
+    public category;
+}
